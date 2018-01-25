30536a045512611b6840989f4c046d0b03e7e5b7
Leah974 <lea_heurtin@hotmail.fr>
2018-01-25 14:17:55 +0100

    Test navigation profil
    J'essaye de créer un menu aside qu'on puisse cacher

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+   // SideNav Initialization
+	$(".button-collapse").click(function() {
+
+		// var largeur = $("#slide-out").width();
+		// var largeurfoisdeux = largeur * 2;
+
+		$('#slide-out').animate({
+			width: 'toggle'
+
+		})
+	});
+
+});
+
+
+                
