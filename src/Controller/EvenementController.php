d551cce030c2a5d5454c5b2f6f4239ec994db09f
Leah974 <lea_heurtin@hotmail.fr>
2018-01-26 17:34:42 +0100

    Correction conflit 3

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace App\Controller;
+
 use App\Form\EvenementType;
 use App\Entity\Evenement;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
 class EvenementController extends Controller
 {
     /**
@@ -15,13 +19,17 @@ class EvenementController extends Controller
     {
         $evenement = new Evenement();
         $form = $this->createForm(EvenementType::class, $evenement);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($evenement);
             $em->flush();
+
             return $this->redirectToRoute('profil');
         }
+
         return $this->render(
             'sitepublic/proposer.html.twig',
             array('form' => $form->createView())
