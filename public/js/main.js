d49afa1431e741f7d512219c7fd547923407922a
Leah974 <lea_heurtin@hotmail.fr>
2018-01-26 17:02:07 +0100

    Menu modal profil
    Création d'un menu pour le profil en modal.
    Le contenu de chaque lien du menu s'affiche dynamiquement.

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,6 @@
 $(document).ready(function(){
 
 
-   // SideNav Initialization
-	// $("#btn-nav-profil").click(function() {
-
-	// 	$('#profil-nav').animate({ 
-	// 		right : "toggle"
-	// 	}, "slow");
-			
-	// });
-	
-	$('#profil-nav').hide();
-
-	// SideNav Initialization
-	$("#btn-nav-profil").click(function() {
-
-		// var largeur = $('#profil-nav').width();
-
-		$('#profil-nav').show();
-			
-	});
-
-	$("#profil-content").click(function() {
-
-		// var largeur = $('#profil-nav').width();
-
-		$('#profil-nav').hide();
-			
-	});
-
 });
 
 
