26162151079612c92ae94779dab32b0bc6a01e02
Vifi34 <fillolvirginie@gmail.com>
2018-01-28 17:34:57 +0100

    Evenements
    Ajout de commentaires sur EvenementController, création de l’affichage
    de tous les événements publics (route : événements) et de la page
    details événement (route : evenements/{$id} Création d’une page
    erreur.html.twig si l’utilisateur tente d’accéder a un evenement/id qui
    n’existe pas

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -15,46 +15,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class EvenementController extends Controller
 {
     /**
+     * Enregistre les informations de l'événement en bdd
      * @Route("/evenements/proposer", name="proposerevenements")
      */
     public function registerEvenement(Request $request)
     {
         $evenement = new Evenement();
+            // création du formulaire
         $form = $this->createForm(EvenementType::class, $evenement);
-
         $form->handleRequest($request);
 
+            // si le formulaire est rempli et valide
         if ($form->isSubmitted() && $form->isValid()) {
-
+                // vérifie si l'utilisateur est connecté
             $user = $this->getUser();
-
+                // si utilisateur non connecté
             if (!$user) {
-
+                // à la validation du formulaire, renvoie vers la page de connexion
             return $this->redirectToRoute('connexion');
 
             }
-
+                // dossier d'enregistrement de la photo evenement
             $dir = 'img/uploads';
-
+                // recuperation de la photo uploadé et recuperation de l'extension
             $file = $form['photo']->getData();
             $extension = $file->guessExtension();
-
+                // si l'extension n'est pas reconnu on affecte une extension jpeg
             if (!$extension) {
                 // extension cannot be guessed
-                $extension = 'bin';
+                $extension = 'jpeg';
             }
+                // on renomme la photo en 'photoevenement(chiffre entre 1 et 99999).extension'
             $nomPhoto = 'photoevenement'.rand(1, 99999).'.'.$extension;
+                // on deplace la photo dans le dossier
             $file->move($dir, $nomPhoto);
-
+                // on enregistre le nom de la photo en bdd
             $evenement->setPhoto($nomPhoto);
-
+                //récupération du nom utilisateur
             $username = $user->getUsername();
+                // on enregistre le nom utilisateur comme organisateur de l'événement
             $evenement->setOrganisateur($username);
-
+                // enregistrement des infos en bdd
             $em = $this->getDoctrine()->getManager();
             $em->persist($evenement);
             $em->flush();
-
+                // redirection vers la page profil
             return $this->redirectToRoute('profil');
         }
 
@@ -65,6 +70,7 @@ class EvenementController extends Controller
     }
 
     /**
+     * Affiche toutes les informations de l'événement dont l'id = $id
      * @Route("/evenements/{id}", name="detailsEvenements")
      */
     public function showDetailsEvenement($id)
@@ -73,25 +79,28 @@ class EvenementController extends Controller
             ->getRepository(Evenement::class)
             ->find($id);
 
-        if (!$evenement) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            // si il n'y a pas d'événement ou si l'événement id = $id n'existe pas
+        if (!$evenement || !$id)
+        {
+                // renvoie vers une page d'erreur
+            return $this->render('sitepublic/erreurEvenements.html.twig');
         }
 
         return $this->render('sitepublic/details.html.twig', ['evenement' => $evenement]);
     }
 
-    // /**
-    //  * @Route("/evenements", name="evenements")
-    //  */
-    //     public function showEvenement()
-    //     {
-    //     $repository = $this->getDoctrine()->getRepository(Evenement::class);
-    //     $evenement = $repository->findAll();
-
-
-
-    //     return $this->render('sitepublic/evenements.html.twig', ['evenement' => $evenement]);
-    //     }
+    /**
+     * Affiche tous les événements publics (statut = false) rangés par ordre chronologique
+     * @Route("/evenements", name="evenements")
+     */
+        public function showEvenement()
+        {
+                $evenements = $this->getDoctrine()
+                ->getRepository(Evenement::class)
+                ->findBy(
+                    ['statut' => false],
+                    ['dateEvenement' => 'ASC']
+                );
+        return $this->render('sitepublic/evenements.html.twig', ['evenements' => $evenements]);
+        }
 }
